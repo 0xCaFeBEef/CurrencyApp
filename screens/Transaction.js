3dcd6c397fa8d30cb22f75104c183bfa5d7398dc
Melih Şahin <lestonz@hotmail.com>
2021-11-25 19:57:29 +0300

    Crypto Currency App
    Cypto Currency App, I made it with ByProgrammers Youtube Channel.
    This is link: https://www.youtube.com/watch?v=xBmx2eaozck&t=2059s

---
+++ screens/Transaction.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import {
+    StyleSheet,
+    SafeAreaView,
+    ScrollView,
+    View,
+    Text,
+    LogBox
+} from 'react-native';
+
+import { HeaderBar, CurrencyLabel, TextButton, TransactionHistory } from '../components';
+import { dummyData, COLORS, SIZES, FONTS, icons } from "../constants";  
+
+const Transaction = ({route }) => {
+
+    React.useEffect(() => {
+        LogBox.ignoreLogs(['VirtualizedLists should never be nested'])
+    }, [])
+    const [selectedCurrency, setSelectedCurrency] = React.useState(null)
+
+    React.useEffect(() => {
+        const {currency} = route.params
+        setSelectedCurrency(currency)
+    })
+    function renderTrade() {
+        return (
+            <View
+                style={{
+                    marginTop: SIZES.padding,
+                    marginHorizontal: SIZES.padding,
+                    padding: SIZES.padding,
+                    borderRadius: SIZES.radius,
+                    backgroundColor: COLORS.white,
+                    ...styles.shadow
+                }}
+            >
+                <CurrencyLabel 
+                    icon={selectedCurrency?.image}
+                    currency={selectedCurrency?.currency}
+                    code={selectedCurrency?.code}
+                />
+                <View
+                    style={{
+                        marginTop: SIZES.padding,
+                        marginBottom: SIZES.padding * 1.5,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Text style={{...FONTS.h2}} > {selectedCurrency?.wallet.crypto}{selectedCurrency?.code} </Text>
+                    <Text style={{...FONTS.body4, color: COLORS.gray}} >{selectedCurrency?.wallet.value}</Text>
+                </View>
+                <TextButton 
+                    label="Trade"
+                    onPress={() => console.log("Trade")}
+                />
+            </View>
+        )
+    }
+    function renderTransactionHistory() {
+        return (
+            <TransactionHistory 
+                customContainerStyle={{
+                    ...styles.shadow
+                }}
+                history={selectedCurrency?.transactionHistory}
+            />
+        )
+    }
+
+ 
+    return (
+       <SafeAreaView style={{flex: 1}} >
+           <HeaderBar 
+                right={false}
+           />
+           <ScrollView>
+               <View
+                    style= {{
+                        flex:1, 
+                        paddingBottom: SIZES.padding
+                    }}
+               >
+                   {renderTrade()}
+                   {renderTransactionHistory()}
+               </View>
+           </ScrollView>
+       </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+
+        elevation: 8,
+    }
+})
+
+export default Transaction;
